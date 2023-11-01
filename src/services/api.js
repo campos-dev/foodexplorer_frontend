2118b71f39915ee510c9554116281040d3310080
Marcelo Campos <camposwebdev@gmail.com>
2023-11-01 20:42:26 -0300

    netlify proxy to fix cors Access-Control-Allow-Origin

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://explorer-foodexplorer.onrender.com",
+  baseURL: "https://explorer-foodexplorer.netlify.app/api",
   withCredentials: true,
 });
