3817a331142c2a4e01312fce95cb0ff7a1bf265c
Marcelo Campos <camposwebdev@gmail.com>
2023-11-03 18:09:56 -0300

    remove proxy netlify.toml

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://explorer-foodexplorer.netlify.app/api",
+  baseURL: "https://explorer-foodexplorer.onrender.com",
   withCredentials: true,
 });
